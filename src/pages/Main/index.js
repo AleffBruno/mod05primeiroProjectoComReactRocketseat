f165bd6ee9198707004e23f07e988c9096a8a7fa
Aleff <aleffbruno@gmail.com>
2019-11-28 18:44:55 -0300

    add react-hooks in Main/index.js

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
@@ -7,45 +7,67 @@ import api from '../../services/api';
 import Container from '../../components/Container';
 import { Form, SubmitButton, List } from './styles';
 
-export default class Main extends Component {
-    state = {
-        newRepo: '',
-        repositories: [],
-        loading: false,
-        errorRequest: false
-    }
+export default function Main() {
+    // state = {
+    //     newRepo: '',
+    //     repositories: [],
+    //     loading: false,
+    //     errorRequest: false
+    // }
 
-    componentDidMount() {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorRequest, setErrorRequest] = useState(false);
+
+    useEffect(() => {
         const repositories = localStorage.getItem('repositories');
 
         if (repositories) {
-            this.setState({
-                repositories: JSON.parse(repositories)
-            })
+            setRepositories(JSON.parse(repositories))
+            // this.setState({
+            //     repositories: JSON.parse(repositories)
+            // })
         }
+    }, [])
+
+    // componentDidMount() {
+    //     const repositories = localStorage.getItem('repositories');
+
+    //     if (repositories) {
+    //         this.setState({
+    //             repositories: JSON.parse(repositories)
+    //         })
+    //     }
+    // }
+
+    useEffect(() => {
+        localStorage.setItem('repositories',JSON.stringify(repositories))
+    }, [repositories])
+
+    // componentDidUpdate(_,prevState) {
+    //     const { repositories } = this.state;
+
+    //     if (prevState.repositories !== repositories) {
+    //         localStorage.setItem('repositories',JSON.stringify(repositories))
+    //     }
+    // }
+
+    function handleInputChange(e) {
+        setNewRepo(e.target.value)
+        // this.setState({
+        //     newRepo: e.target.value,
+        // })
     }
 
-    componentDidUpdate(_,prevState) {
-        const { repositories } = this.state;
-
-        if (prevState.repositories !== repositories) {
-            localStorage.setItem('repositories',JSON.stringify(repositories))
-        }
-    }
-
-    handleInputChange = e => {
-        this.setState({
-            newRepo: e.target.value,
-        })
-    }
-
-    handleSubmit = async e => {
+    async function handleSubmit(e) {
         e.preventDefault();
 
         try {
-            this.setState({ loading: true })
+            // this.setState({ loading: true })
+            setLoading(true);
 
-            const { newRepo, repositories } = this.state;
+            // const { newRepo, repositories } = this.state;
     
             const response = await api.get(`/repos/${newRepo}`)
     
@@ -57,61 +79,67 @@ export default class Main extends Component {
                 throw new Error('Repositório duplicado');
             }
     
-            this.setState({
-                repositories: [...repositories,data],
-                newRepo: '',
-                loading: false,
-                errorRequest: false,
-            })
+            setRepositories([...repositories,data]);
+            setNewRepo('');
+            setLoading(false);
+            setErrorRequest(false);
+            // this.setState({
+            //     repositories: [...repositories,data],
+            //     newRepo: '',
+            //     loading: false,
+            //     errorRequest: false,
+            // })
     
         } catch (e) {
-            this.setState({
-                errorRequest: true
-            })
+            setErrorRequest(true);
+            // this.setState({
+            //     errorRequest: true
+            // })
             console.log('deu ruim', e);
         }
         
-        this.setState({
-            loading: false
-        })
+        setLoading(false);
+        // this.setState({
+        //     loading: false
+        // })
     }
 
-    render() {
-        const { newRepo, repositories, loading, errorRequest } = this.state;
-        return (
-        <Container>
-            <h1>
-                <FaGithubAlt />
-                Repositorios
-            </h1>
+    
+    // const { newRepo, repositories, loading, errorRequest } = this.state;
+    return (
+    <Container>
+        <h1>
+            <FaGithubAlt />
+            Repositorios
+        </h1>
 
-            <Form onSubmit={this.handleSubmit} errorRequest={errorRequest}>
-                <input
-                    type="text"
-                    placeholder="Add repository"
-                    value={newRepo}
-                    onChange={this.handleInputChange}
-                />
+        <Form onSubmit={handleSubmit} errorRequest={errorRequest}>
+            <input
+                type="text"
+                placeholder="Add repository"
+                value={newRepo}
+                onChange={handleInputChange}
+            />
+            
+            <SubmitButton loading={loading}>
+                { loading ? (
+                    <FaSpinner color="#FFF" size={14}></FaSpinner>
+                ) : (
+                    <FaPlus color="#FFF" size={14}></FaPlus>
+                )}
                 
-                <SubmitButton loading={loading}>
-                    { loading ? (
-                        <FaSpinner color="#FFF" size={14}></FaSpinner>
-                    ) : (
-                        <FaPlus color="#FFF" size={14}></FaPlus>
-                    )}
-                    
-                </SubmitButton>
-            </Form>
+            </SubmitButton>
+        </Form>
 
-            <List>
-                {repositories.map(repository => (
-                    <li key={repository.name}>
-                        <span>{repository.name}</span>
-                        <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
-                    </li>
-                ))}
-            </List>
-        </Container>
-        )
-    }  
+        <List>
+            {repositories.map(repository => (
+                <li key={repository.name}>
+                    <span>{repository.name}</span>
+                    <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+                </li>
+            ))}
+        </List>
+    </Container>
+    )
+    
 }
